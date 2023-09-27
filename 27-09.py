203f5b48bc0f1c8e21f72e366f9ae6da0103da53
aayushker <singhaayushker@gmail.com>
2023-09-28 01:26:01 +0530

    added files

---
+++ 27-09.py
@@ -0,0 +1,35 @@
+# 27/09/2023 
+
+# a=int(input("Enter the element"))
+# b=int(input("Enter the element"))
+# while True :
+#     op=input("Enter the operator or 0 to end")
+#     if (op=='+'):
+#         print("Sum=",a+b)
+#     elif( op=='-'):
+#         print("Sub=",a-b)
+#     elif (op=='*'):
+#         print("Multiplication=",a*b)
+#     elif (op=='/'):
+#         print("Multiplication=",a/b)
+#     elif (op=='0'):
+#         print("End")
+#         break
+    
+# a= input("Enter the character")
+# if (a>="A" and a<="Z"):
+#     print("Upper case")
+# elif (a>="a" and a<="z"):
+#     print("Lower Case")
+# else:
+#     print("Error")
+
+# a= int(input("Enter first number"))
+# b= int(input("Enter second number"))
+# c= int(input("Enter third number"))
+# if (a>b and a>c):
+#     print("Greatest is",a)
+# elif (b>a and b>c):
+#     print("Greatest is:",b)
+# elif (c>a and c>b):
+#     print("Greatest is:",c)
---
+++ hello.py
@@ -0,0 +1,11 @@
+# a=int(input("Enter the element "))
+# for i in range(1,a+1):
+#     print(i,end=" ")
+# i=0
+# while i<a:
+#     print("Hello",i)
+#     i+=1
+
+
+
+
